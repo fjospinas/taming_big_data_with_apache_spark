fce6484f097965ed06f8c41d4ebb080b9a779665
Francisco Ospina <francisco.ospina@gmail.com>
2018-05-14 10:07:36 -0500

    Add second exercise

---
+++ src/spark basics/total_amounts_sorted.py
@@ -0,0 +1,27 @@
+import os
+from pyspark import SparkContext, SparkConf
+from pathlib import Path
+
+
+def parse_line(x):
+    fields = x.split(',')
+    id = fields[0]
+    amount = float(fields[2])
+    return id, amount
+
+
+data_path = os.path.join(str(Path(__file__).parents[2]), 'data', 'customer-orders.csv')
+
+conf = SparkConf().setAppName('TotalAmountsApp')
+sc = SparkContext(conf=conf)
+
+text_file = sc.textFile(data_path)
+
+all_build = text_file.map(lambda x: parse_line(x))
+
+sums = all_build.reduceByKey(lambda a, b: a + b)\
+
+sums = sums.map(lambda (x, y): (y, x)).sortByKey().collect()
+
+for tup in sums:
+    print(tup[1], '%.2f' % tup[0])
